05234ca3cf7f8b0869b395810da20253e01886d4
Bop <kingbeebop@gmail.com>
2024-10-01 14:48:15 -0400

    define main file operations

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+mod db;
+mod models;
+mod routes;
+
+use routes::{get_tasks, create_task};
+use rocket::fairing::AdHoc;
+use sqlx::PgPool;
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .attach(AdHoc::on_ignite("Database", |rocket| async {
+            let pool = db::establish_connection().await;
+            rocket.manage(pool)
+        }))
+        .mount("/", routes![get_tasks, create_task])
 }
